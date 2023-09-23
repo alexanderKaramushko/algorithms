c02aecfcad265b674294c7ca43aa8ee7b1b49bf9
alex.karamushko <alexandr.karamushko@cinimex.ru>
2023-09-23 16:05:59 +0400

    add create_subset

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "calc_mod.cpp"
 
 using namespace std;
 
@@ -9,13 +8,6 @@ int main()
 {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  int x;
-  int m;
-
-  cin >> x >> m;
-
-  calc_mod(x, m);
 
   return 0;
 }
